255cf1db403ee92f133ab843fc3f333d04149abb
Luigi Bolovan <luigibolovan@gmail.com>
2020-12-26 20:18:36 +0200

    prepare test env for actual version

--- src/test/java/ro/upt/vvs/notesapp/mockito/NoteMockDao.java
+++ src/test/java/ro/upt/vvs/notesapp/mockito/NoteMockDao.java
@@ -1,63 +1,136 @@
-//package ro.upt.vvs.notesapp.mockito;
-//
-//import ro.upt.vvs.notesapp.dao.NoteDao;
-//import ro.upt.vvs.notesapp.model.Note;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//import java.util.UUID;
-//
-//public class NoteMockDao implements NoteDao {
-//    private List<Note> mockNotes = new ArrayList<>();
-//
-//    public NoteMockDao() {
-//        mockNotes.add(new Note(UUID.randomUUID(), "Note1"));
-//        mockNotes.add(new Note(UUID.randomUUID(), "Note2"));
-//        mockNotes.add(new Note(UUID.randomUUID(), "Note3"));
-//        mockNotes.add(new Note(UUID.randomUUID(), "Note4"));
-//    }
-//
-//    @Override
-//    public int insertNote(UUID id, Note note) {
-//        mockNotes.add(new Note(id, note.getText()));
-//        return 0;
-//    }
-//
-//    @Override
-//    public List<Note> getAllNotes() {
-//        return mockNotes;
-//    }
-//
-//    @Override
-//    public int deleteNoteById(UUID id) {
-//        for(Note note : mockNotes){
-//            if(note.getId().equals(id)){
-//                mockNotes.remove(note);
-//                return 0;
-//            }
-//        }
-//        return 1;
-//    }
-//
-//    @Override
-//    public int updateNoteById(UUID id, Note note) {
-//        for(Note mynote : mockNotes) {
-//            if (mynote.getId().equals(id)) {
-//                int index = mockNotes.indexOf(mynote);
-//                mockNotes.set(index, note);
-//                return 0;
-//            }
-//        }
-//        return 1;
-//    }
-//
-//    @Override
-//    public Optional<Note> getNoteById(UUID id) {
-//        return mockNotes.stream()
-//                .filter(note -> note.getId().equals(id))
-//                .findFirst();
-//    }
-//
-//
-//}
+package ro.upt.vvs.notesapp.mockito;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import ro.upt.vvs.notesapp.dao.NoteDao;
+import ro.upt.vvs.notesapp.model.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class NoteMockDao implements NoteDao {
+    @Override
+    public Optional<Note> findById(String s) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(String s) {
+        return false;
+    }
+
+    @Override
+    public List<Note> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Note> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<Note> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Note> findAllById(Iterable<String> iterable) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(String s) {
+
+    }
+
+    @Override
+    public void delete(Note note) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Note> iterable) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public <S extends Note> S save(S s) {
+        return null;
+    }
+
+    @Override
+    public <S extends Note> List<S> saveAll(Iterable<S> iterable) {
+        return null;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends Note> S saveAndFlush(S s) {
+        return null;
+    }
+
+    @Override
+    public void deleteInBatch(Iterable<Note> iterable) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public Note getOne(String s) {
+        return null;
+    }
+
+    @Override
+    public <S extends Note> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Note> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends Note> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends Note> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Note> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Note> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    private List<Note> mockNotes = new ArrayList<>();
+}
